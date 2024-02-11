041a423e143edb5c89468e8145c4e3063e1c85e9
JP.Zhou <3214423826@qq.com>
2024-02-11 21:33:09 +0800

    修改了settings.js文件中的内容并添加了注释

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,19 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  // 网站标题
+  title: '人力资源后台管理系统',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  // 是否固定头部
+  fixedHeader: true,
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false
+  // 是否显示侧边栏的logo
+  sidebarLogo: true
 }
