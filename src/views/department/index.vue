0dea232dcba4c216fba9fb04c7a5e3a8af1e6bb6
JP.Zhou <3214423826@qq.com>
2024-02-20 13:10:22 +0800

    自定义结构、获取作用域插槽数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,29 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree default-expand-all="true" :data="depts" :props="defaultProps" />
+      <el-tree default-expand-all="true" :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">
+                {{ data.managerName }}
+              </span>
+            </el-col>
+            <el-dropdown>
+              <span class="el-dropdown-link">
+                下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>添加子部门</el-dropdown-item>
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -12,7 +34,7 @@ export default {
   data() {
     return {
       depts: [
-        { name: '传智教育', children: [{ name: '总裁办' }, { name: '行政部' }, { name: '人事部' }] }
+        { name: '传智教育', managerName: '管理员', children: [{ name: '总裁办', managerName: '张三' }, { name: '行政部', managerName: '李四' }, { name: '人事部', managerName: '王五' }] }
       ],
       defaultProps: {
         label: 'name',
@@ -27,5 +49,11 @@ export default {
 .app-container {
   padding: 30px 140px;
   font-size: 14px;
+}
+
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>
