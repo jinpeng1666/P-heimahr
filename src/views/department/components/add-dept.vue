76ae644d8d704688deff9c7c80f0b9d03feda909
JP.Zhou <3214423826@qq.com>
2024-02-20 17:36:17 +0800

    添加子部门-新建弹层组件

---
+++ src/views/department/components/add-dept.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-dialog title="新增部门" @close="close" :visible="showDialog">
+
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    close() {
+      this.$emit('update:showDialog', false)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree default-expand-all="true" :data="depts" :props="defaultProps">
+      <el-tree :expand-on-click-node="false" default-expand-all="true" :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
         <template v-slot="{ data }">
           <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
@@ -12,30 +12,34 @@
                 {{ data.managerName }}
               </span>
             </el-col>
-            <el-dropdown>
+            <el-dropdown @command="operateDept">
               <span class="el-dropdown-link">
-                下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+                下拉菜单<i class="el-icon-arrow-down el-icon--right" />
               </span>
               <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>添加子部门</el-dropdown-item>
-                <el-dropdown-item>编辑部门</el-dropdown-item>
-                <el-dropdown-item>删除</el-dropdown-item>
+                <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                <el-dropdown-item command="del">删除</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
           </el-row>
         </template>
       </el-tree>
     </div>
+    <add-dept :show-dialog.sync="showDialog" />
   </div>
 </template>
 <script>
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
+import AddDept from './components/add-dept.vue'
 
 export default {
   name: 'Department',
+  components: { AddDept },
   data() {
     return {
+      showDialog: false,
       depts: [],
       defaultProps: {
         label: 'name',
@@ -50,6 +54,9 @@ export default {
     async getDepartment() {
       const result = await getDepartment()
       this.depts = transListToTreeData(result, 0)
+    },
+    operateDept(type) {
+      this.showDialog = true
     }
   }
 }
