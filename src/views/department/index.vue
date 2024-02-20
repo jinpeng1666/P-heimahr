41407b7ce517793d0634a9a6575aa26fbd078813
JP.Zhou <3214423826@qq.com>
2024-02-20 12:23:55 +0800

    树形组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <!-- 展示树形结构 -->
+      <el-tree default-expand-all="true" :data="depts" :props="defaultProps" />
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [
+        { name: '传智教育', children: [{ name: '总裁办' }, { name: '行政部' }, { name: '人事部' }] }
+      ],
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      }
+    }
+  }
 }
 </script>
+
+<style scpoe>
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
