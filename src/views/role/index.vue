a5a00f39290c284ffa7dd1fabce7f20bd6d37052
JP.Zhou <3214423826@qq.com>
2024-02-23 12:13:11 +0800

    角色管理-完成添加角色弹层的表单校验

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -29,15 +29,15 @@
       </el-row>
       <!-- 放置弹层 -->
       <el-dialog width="500px" :visible.sync="showDialog" title="新增角色">
-        <el-form label-width="120px">
-          <el-form-item label="角色名称">
-            <el-input style="width: 300px" size="mini" />
+        <el-form ref="roleForm" :model="roleForm" :rules="rules" label-width="120px">
+          <el-form-item prop="name" label="角色名称">
+            <el-input v-model="roleForm.name" style="width: 300px" size="mini" />
           </el-form-item>
           <el-form-item label="启用">
-            <el-switch size="mini" />
+            <el-switch :active-value="1" :inactive-value="0" v-model="roleForm.state" size="mini" />
           </el-form-item>
-          <el-form-item label="角色描述">
-            <el-input type="textarea" :rows="3" style="width: 300px" size="mini" />
+          <el-form-item prop="description" label="角色描述">
+            <el-input v-model="roleForm.description" type="textarea" :rows="3" style="width: 300px" size="mini" />
           </el-form-item>
           <el-form-item>
             <el-row type="flex" justify="center">
@@ -65,6 +65,15 @@ export default {
         page: 1,
         pagesize: 5,
         total: 0
+      },
+      roleForm: {
+        name: '',
+        description: '',
+        state: 0
+      },
+      rules: {
+        name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
+        description: [{ required: true, message: '角色描述不能为空', trigger: 'blur' }]
       }
     }
   },
