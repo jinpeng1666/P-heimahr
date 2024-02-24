cb21b9ddcee48bb8e89d8be5d9e60081376a85af
JP.Zhou <3214423826@qq.com>
2024-02-24 11:47:20 +0800

    管理部门-完成展示员工的分页功能

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -42,7 +42,7 @@
         </el-table>
         <!-- 分页 -->
         <el-row style="height: 60px" align="middle" type="flex" justify="end">
-          <el-pagination layout="total, prev, pager, next" :total="1000" />
+          <el-pagination layout="total, prev, pager, next" :total="total" :current-page="queryParams.page" :page-size="queryParams.pagesize" @current-change="changePage" />
         </el-row>
       </div>
     </div>
@@ -64,8 +64,11 @@ export default {
         children: 'children'
       },
       queryParams: {
-        departmentId: null
+        departmentId: null,
+        page: 1,
+        pagesize: 10
       },
+      total: 0,
       list: []
     }
   },
@@ -83,11 +86,17 @@ export default {
     },
     selectNode(node) {
       this.queryParams.departmentId = node.id
+      this.queryParams.page = 1
       this.getEmployeeList()
     },
     async getEmployeeList() {
-      const { rows } = await getEmployeeList(this.queryParams)
+      const { rows, total } = await getEmployeeList(this.queryParams)
       this.list = rows
+      this.total = total
+    },
+    changePage(newPage) {
+      this.queryParams.page = newPage
+      this.getEmployeeList()
     }
   }
 }
